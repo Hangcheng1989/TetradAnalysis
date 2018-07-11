b3fdf40f55d6afd62eed344a344835c1919c7928
Hangcheng1989 <37592789+Hangcheng1989@users.noreply.github.com>
2018-07-11 13:58:35 -0400

    Update NestedOrNotFunction.R

--- R/NestedOrNotFunction.R
+++ R/NestedOrNotFunction.R
@@ -1,11 +1,56 @@
 #' NestedOrNot Function
 #'
-#' This function allows you to know two SEM models are nested in terms of tetrads or not.
-#' @param "Samplecov1", "Samplecov2", "FactorModel1", "FactorModel2", "Size1" and "Size2" needed for this function.
+#' "NestedOrNot " is used to know two models are nested in terms of tetrads or not (Bollen, 1993).
+#' @param "Data1", An optional data frame containing the observed variables used in the first model.
+#' @param "Data2", An optional data frame containing the observed variables used in the second model.
+#' @param "Model1" A description of the user-specified first model. The model is described using the lavaan model synta(Rosseel, 2012).
+#' @param "Model2" A description of the user-specified second model. The model is described using the lavaan model synta(Rosseel, 2012).
+#' @param "Size1" Size of the first data sample.
+#' @param "Size2" Size of the second data sample.
+
 #' @keywords Nested or not in Confirmatory Tetrad Analysis
+
+#' @return "Model1COV",    Implied variance-covariance matrix from the first model.
+#' @return "Model2COV",    Implied variance-covariance matrix from the second model.
+#' @return "NRVT1",     Nonredundant vanishing tetrad setting implied by the first model.
+#' @return "NRVT2",     Nonredundant vanishing tetrad setting implied by the second model.
+#' @return "NRVT_Num1",     Nonredundant vanishing tetrad number for the first model.
+#' @return "NRVT_Num1",     Nonredundant vanishing tetrad number for the second model.
+#' @return "NEST",     Two models are nested or not nested in terms of tetrads.
+
+
+#' @references Bollen, K. A., & Ting, K. F. (1993). Confirmatory tetrad analysis. Sociological methodology, 147-175.
+#' @references Rosseel, Y. (2012). lavaan: An R package for structural equation modeling. Journal of Statistical Software, 48(2), 1-36.
+
 #' @export
 #' @examples
-#' NestedOrNot()
+#' ## Input sample variance-covariance matrix "(Sampledata) ".
+#' data<-c(2.18, 0.63, 0.50, 0.22, 0.39, 0.42, 0.63, 1.44, 0.50, 0.12, 0.11, 0.15, 0.50, 0.50, 1.71, 0.87, 0.59, 0.48, 0.22, 0.12, 0.87, 1.46, 0.54, 0.42, 
+#'	0.39, 0.11, 0.59, 0.54, 1.12, 0.23, 0.42, 0.15, 0.48, 0.42, 0.23, 1.42)
+#' Sampledata <- matrix(data, nrow = 6, ncol = 6, byrow = TRUE)
+#' colnames(Sampledata) <- c('x1','x2','x3','x4','x5','x6')
+#'
+#' ## Sample size
+#' Size<-100
+#'
+#' ## Input model
+#' FactorModel1<-'
+#' Y1 =~ x1 + x2 + x3
+#' Y2 =~ x4 + x5 + x6
+#' Y2 ~ Y1
+#' x3 ~ x4
+#' '
+#'
+#'FactorModel2<-'
+#'Y1 =~ x1 + x2 + x3
+#'Y2 =~ x4 + x5 + x6
+#'Y2 ~ Y1
+#'x4 ~~ x3
+#''
+#'
+#' ## Use "NestedOrNot " to run Confirmatory Tetrad Analysis
+#' NestedOrNot(Sampledata,FactorModel1,Size,Sampledata,FactorModel2,Size)
+
 
 
 ##################################################################################
